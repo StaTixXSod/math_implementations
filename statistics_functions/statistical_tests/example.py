0628e17be94bc67cc1355d48aa8995c6d2086778
StatixXSod <pav-95@yandex.ru>
2022-07-22 16:03:21 +0300

    add two sample ttest

--- statistics_functions/statistical_tests/example.py
+++ statistics_functions/statistical_tests/example.py
@@ -1,10 +1,11 @@
 from student_test import *
+from scipy.stats import ttest_rel, ttest_ind
 import numpy as np
 np.random.seed(43)
 
 v1 = np.random.standard_t(100, 1000)
+v2 = np.random.standard_t(100, 1000)
 # rvs = stats.norm.rvs(loc=5, scale=10, size=(50, 2), random_state=42)
-v1 = list(v1)
 
 # print(one_sample_ttest(v1, v2))
 # print(stats.ttest_ind(v1, v2))
@@ -12,9 +13,13 @@ v1 = list(v1)
 # print(t_value(10.8, 10, 2, 25))
 # print(paired_ttest_simp(89.9, 80.7, 11.3, 11.7, 20, 20))
 
+print(paired_ttest(v1, v2))
+print(ttest_rel(v1, v2))
+
+# print(two_sample_ttest(v1, v2))
+# print(ttest_ind(v1, v2))
+
 # qqplot(v1)
 
 # print(np.median(v1))
 # print(median(v1))
-print(var(v1))
-print(np.var(v1))
--- statistics_functions/statistical_tests/student_test.py
+++ statistics_functions/statistical_tests/student_test.py
@@ -4,11 +4,11 @@ import sys
 sys.path.append(os.getcwd())
 from statistics_functions.functions import *
 
-def one_sample_ttest(v1, v2) -> float:
-    """Return T test value for population data
+def one_sample_ttest(a: list, popmean: float) -> float:
+    """Return T test statistic value for population data
     and sample data
 
-    INFO:
+    Info:
     -----
     This test compare the population and the sample data. 
     Usually use, when we have general data and we want to know, 
@@ -17,9 +17,9 @@ def one_sample_ttest(v1, v2) -> float:
     This test shows, how far our sample mean (X) deviates from 
     the population data mean (M).
 
-    FORMULA:
+    Formula:
     --------
-    >>> t = (M - X) / (SE)
+    `t = (M - X) / (SE)`
         where:
             M: population mean
             X: sample mean
@@ -28,70 +28,126 @@ def one_sample_ttest(v1, v2) -> float:
     Here we use SE of sample data, because usually we can't 
     calculate SE for population data, because we don't know the STD of population data.
 
+    -----
     Args:
         population (list): population data
         sample (list): sample data
     """
-    pop_mean = mean(v1)
-    sample_mean = mean(v2)
+    sample_mean = mean(a)
 
-    t = (pop_mean - sample_mean) / standard_error(v2)
+    t = (popmean - sample_mean) / standard_error(a)
     return t
 
-def two_sample_ttest(v1, v2):
-    pass
+def two_sample_ttest(a: list, b: list) -> float:
+    """Return T-test statistic value for two samples
 
-def paired_ttest(v1: list, v2: list) -> float:
-    """Return T Student Criterion statistic value
-    
-    INFO:
+    Info:
     -----
-    This criterion allows to compare 2 samples of data
-    and called "Paired TTest" or "T-Student Criterion"
+    We assume that we have 2 independent samples but their means are identical.
+    As opposed to pairwise comparison in 'paired T-test', here we compare the means
+    of 2 samples, divided by their SE (not really, but it seems like SE).
+
+    If we have just 2 independent samples of data and we want to compare their mean values,
+    we can just use 'two_sample_ttest'. If we have data before and after some experiment, 
+    then this data is 'dependent' and it's better to use paired ttest.
 
     Hypothesis:
+    -----------
     The null hypothesis (H0): there is no difference between this 2 sample means
         H0: M1 = M2
     The alternative hypothesis (H1): this 2 sample means NOT EQUAL to each other
     and there is a good chances to get some great results
         H1: M1 != M2
 
-    It would be good to get H1.
-
-    FORMULA:
+    Formula:
     --------
-    t = (X1 - X2) - (M1 - M2) / SQRT( (sd1^2 / n1) + (sd2^2 / n2) )
+    `t = (X1 - X2) - (M1 - M2) / SQRT( (sd1^2 / n1) + (sd2^2 / n2) )`
 
     Suppose that M1 - M2 = 0, because we assume in population data M1 and M2 are identical,
     so we can rewrite the formula:
 
-    t = (X1 - X2) / SQRT( (sd1^2 / n1) + (sd2^2 / n2) )
+    `t = (X1 - X2) / SQRT( (sd1^2 / n1) + (sd2^2 / n2) )`
 
-    INTERPRETATION:
+    Interpretation:
     ---------------
     This value says, that our difference between 2 sample means deviates 
     from the population mean on "t" sigma value.
 
-    NOTE:
     -----
-    1. To use T Test, it is better that variance from this 2 samples
-    were approximately the same (dispersion homogeneity requirement). 
-    To check this requirement use "Levene test" or "Fisher criterion".
-    2. If NO (number of observations) of sample data is not really big (NO < 30), this is very important
-    to have normal distribution of sample data. If NO > 30, this criterion is fine
-    even without normal distribution. (But it's steel better to have normal distribution)
-
     Args:
-        v1 (list): 1st list of observations
-        v2 (list): 2nd list of observations
+        a (list): first independent series data
+        b (list): second independent series data
+
+    Returns:
+        float: statistic value t
     """
-    X1, X2 = mean(v1), mean(v2)
-    sd1, sd2 = std(v1), std(v2)
-    n1, n2 = len(v1), len(v2)
+    X1, X2 = mean(a), mean(b)
+    sd1, sd2 = std(a), std(b)
+    n1, n2 = len(a), len(b)
 
     se1 = sd1**2 / n1
     se2 = sd2**2 / n2
 
     t = (X1 - X2) / (se1 + se2)**0.5
 
+    return t
+
+def paired_ttest(a: list, b: list) -> float:
+    """Return T Student Criterion statistic value
+    
+    Info:
+    -----
+    This criterion allows to compare 2 samples of data
+    and called "Paired TTest" or "T-Student Criterion". Paired T-test is used when 
+    2 data samples is from one population data.
+    Paired samples t-tests are often referred to as "dependent samples t-tests". 
+    This is because here we calculate the difference element by element, then calculate
+    the mean and stadard error of this difference. So this test can be use only, if we 
+    have some data before experiment and after experiment.
+
+    Usage example:
+    --------------
+    We can use paired t-test if we are interested in the difference between
+    two variables for the same subject. It can be cholesterol level measurement 
+    for xxxx year and 1 year later. 
+
+    If we have the group of data before and after experiment, we can use paired T-test.
+
+    Hypothesis:
+    -----------
+    * H0: The mean pre-test and post-test scores are equal
+    * H1: The mean pre-test and post-test scores are not equal
+
+    Formula:
+    --------
+    `t = Md / SE(Md)`
+        where
+            Md: The mean difference between a and b samples
+            SE: Standard error
+
+    Note:
+    -----
+    1. To use T Test, it is better that variance from this 2 samples
+    were approximately the same (dispersion homogeneity requirement). 
+    To check this requirement use "Levene test" or "Fisher criterion".
+    2. If NO (number of observations) of sample data is not really big (NO < 30), this is very important
+    to have normal distribution of sample data. If NO > 30, this criterion is fine
+    even without normal distribution. (But it's steel better to have it tho)
+
+    Steps:
+    ------
+    1. Calculate the pairwise difference between a and b sample lists (d)
+    2. Calculate the mean of the difference (Md)
+    3. Calculate the standard error of difference (SE)
+    4. Divide Md by SE
+
+    -----
+    Args:
+        a (list): pre-test data
+        b (list): post-test data
+    """
+    paired_differences = paired_diff(a, b)
+    mean_differences = mean(paired_differences)
+    SE = standard_error(paired_differences)
+    t = mean_differences / SE
     return t
