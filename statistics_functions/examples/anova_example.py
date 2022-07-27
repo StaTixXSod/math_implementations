7ee7bafb36fcc1256d7cf73135ae53e9275a80c8
StaTixXSod <pav-95@yandex.ru>
2022-07-27 15:34:11 +0300

    added two way anova

--- statistics_functions/examples/anova_example.py
+++ statistics_functions/examples/anova_example.py
@@ -4,37 +4,51 @@ sys.path.append(os.getcwd())
 
 import statsmodels.api as sm
 from statsmodels.formula.api import ols
-import numpy as np
 
 import pandas as pd
 from statistics_functions.statistical_tests.anova import one_way_anova, two_way_anova
 from scipy.stats import f_oneway
 
-# l1 = [3, 1, 2]
-# l2 = [5, 3, 4]
-# l3 = [7, 6, 5]
+l1 = [3, 1, 2]
+l2 = [5, 3, 4]
+l3 = [7, 6, 5]
 
-# my_f_stat, my_pvalue = one_way_anova(l1, l2, l3)
-# sc_f_stat, sc_pvalue = f_oneway(l1, l2, l3)
+my_f_stat, my_pvalue = one_way_anova(l1, l2, l3)
+sc_f_stat, sc_pvalue = f_oneway(l1, l2, l3)
 
-# print("\n[INFO] One way ANOVA test")
-# print(pd.DataFrame(
-#     {"Statistic": [sc_f_stat, my_f_stat],
-#     "Pvalue": [sc_pvalue, my_pvalue]}
-#     , index=["scipy", "my version"]
-# ))
+print("\n[INFO] One way ANOVA test")
+print(pd.DataFrame(
+    {"Statistic": [sc_f_stat, my_f_stat],
+    "Pvalue": [sc_pvalue, my_pvalue]}
+    , index=["scipy", "my version"]
+))
+
+
+print("\n[INFO] Two way ANOVA test")
+data = pd.read_csv("data/atherosclerosis.csv")
+statistic = two_way_anova(data, features=["age", "dose"], target='expr')
+print(statistic.to_string(index=False))
+print()
+
+model = ols('expr ~ C(age) + C(dose) + C(age):C(dose)', data=data).fit()
+print(sm.stats.anova_lm(model, typ=2))
 
 """
 [INFO] One way ANOVA test
             Statistic  Pvalue
 scipy            12.0   0.008
 my version       12.0   0.008
+
+[INFO] Two way ANOVA test
+ feature       sum_sq df         F    Pvalue
+     age   197.452754  1  7.449841  0.008313
+    dose    16.912241  1  0.638094  0.427552
+age:dose     0.927077  1  0.034978  0.852272
+residual  1590.257424 60      None      None
+
+                     sum_sq    df         F    PR(>F)
+C(age)           197.452754   1.0  7.449841  0.008313
+C(dose)           16.912241   1.0  0.638094  0.427552
+C(age):C(dose)     0.927077   1.0  0.034978  0.852272
+Residual        1590.257424  60.0       NaN       NaN
 """
-
-data = pd.read_csv("data/atherosclerosis.csv")
-print(two_way_anova(data, features=["age", "dose"], target='expr'))
-
-
-model = ols('expr ~ C(age) + C(dose) + C(age):C(dose)', data=data).fit()
-# model = ols('expr ~ C(age) + C(dose)', data=data).fit()
-print(sm.stats.anova_lm(model, typ=2))
--- statistics_functions/statistical_tests/anova.py
+++ statistics_functions/statistical_tests/anova.py
@@ -1,19 +1,20 @@
 import sys, os
 sys.path.append(os.getcwd())
 from typing import Tuple, NamedTuple
-from statistics_functions.functions import *
-from scipy.stats import f
-import pandas as pd
 
-class FactorialAnovaStatistics(NamedTuple):
-    sum_sq: float
-    df: int
-    statistic: float
-    pvalue: float
+import numpy as np
+import pandas as pd
+from scipy.stats import f
+from statistics_functions.functions import *
 
 class OneWayAnovaStatistics(NamedTuple):
     statistic: float
     pvalue: float
+
+class GroupStatistic(NamedTuple):
+    sum_sq: float or list
+    df: int or list
+    ms: float or list
 
 def one_way_anova(*args: list) -> OneWayAnovaStatistics:
     """
@@ -48,8 +49,8 @@ def one_way_anova(*args: list) -> OneWayAnovaStatistics:
 
     Steps:
     ------
-    1. Find sum of squares between groups
-    2. Find sum of squares within groups
+    1. Find the sum of squares between groups
+    2. Find the sum of squares within groups
     3. Calculate the "F" value
 
     Formula:
@@ -196,26 +197,26 @@ def SSW(groups: list) -> Tuple[float, int]:
     ssw = sum([TSS(group) for group in groups])
     return (ssw, df)
 
-def two_way_anova(data: pd.DataFrame, features: list, target: str) -> FactorialAnovaStatistics:
-    """Return the f statistic of data and its pvalue
+def two_way_anova(data: pd.DataFrame, features: list, target: str) -> pd.DataFrame:
+    """Return two-way ANOVA result
 
     Info:
     -----
-    A two-way ANOVA (“analysis of variance”) is used to determine whether
-    or not there is a statistically significant difference between the means 
-    of three or more independent groups that have been split on two variables 
-    (sometimes called “factors”).
+    Two-way ANOVA is almost the same as one-way ANOVA. The difference
+    here is, that for two-way ANOVA we trying to compare all features data with all groups 
+    in all features data. This allows to find out, what feature has the most impact on the data.
+
+    The process pretty the same, as in the One-way ANOVA: find SSB, SSW and calculate the ratio between this 2 values.
 
     Steps:
     ------
-    1. Calculate the mean of all values (grand mean)
-    2. Calculate Sum of Squares for first factor (feature):
-        Calculate the mean for all first feature group.
-        All means substract from the grand mean.
-        Sum all squared differences.
-    3. Calculate Sum of Squares for second factor (feature):
-        The same process - as in step 2.
-    4. Calculate Sum of Squares Within (Error)
+    1. Calculate the sum of squares between groups
+    2. Calculate the sum of squares within groups
+    3. Get total sum of squares
+    4. Calculate interaction of features
+    5. Calculate F values
+    6. Calculate P values
+    7. Form the dataframe with statistic
 
     -----
     Args:
@@ -223,41 +224,136 @@ def two_way_anova(data: pd.DataFrame, features: list, target: str) -> FactorialA
         features (list): the features to compare
 
     Returns:
-        Tuple[float, float]: F statistic and p value
+        Pandas DataFrame: Statistics about the data
     """
-    # 1. Find the sum of squares for each feature
-    statistic, ss_ssb, df_ssb = SSB_pd(data, features, target)
+    # 1. Find the sum of squares for each feature (SSB: Sum of squares between groups)
+    ssb = SSB_Factorial(data, features, target)
 
-    # 2. Find the sum of squares within (SSW) (Error)
-    statistic, ss_ssw, df_ssw = SSW_pd(statistic, data, features, target)
+    # 2. Find the sum of squares within groups SSW (residuals)
+    ssw = SSW_Factorial(data, features, target)
 
     # 3. Calculate total sum of squares
-    ss_total = TSS(data["expr"].values)
+    ss_total = TSS(data[target].values)
 
     # 4. Calculate Sum of Squares Interaction
-    statistic, ss_interaction, df_interaction = interaction(statistic, ss_total, ss_ssb, df_ssb, ss_ssw, features)
-    
+    interactions = interaction(ss_total, ssb, ssw)
+
     # 5. Calculate F values
-    ms_ssb = [ssb_i / df_ssb_i for ssb_i, df_ssb_i in zip(ss_ssb, df_ssb)]
-    ms_ssw = ss_ssw / df_ssw
-    ms_interaction = ss_interaction / df_interaction
-    f_features = [i / ms_ssw for i in ms_ssb]
-    f_interaction = ms_interaction / ms_ssw
-    print(f_features)
-    print(f_interaction)
+    f_values = calculate_F_values(ssb, ssw, interactions)
 
-    # 6. Pvalue
-    p_features = [f.sf(f_feature, df_ssb, df_ssw) for f_feature in f_features]
-    p_interaction = f.sf(f_interaction, df_ssb, df_ssw)
-    print(p_features)
-    print(p_interaction)
+    # 6. Calculate P values
+    p_values = calculate_P_values(f_values, ssb, ssw)
     
-    # Take all together
-    statistic = pd.DataFrame(statistic)
-    print("[INFO] The difference between group mean and gen mean")
-    print(statistic)
+    # 7. Group it all together
+    statistic = form_statistic(ssb, ssw, interactions, f_values, p_values, features)
+    
+    return statistic
 
-def SSW_pd(statistic: dict, data: pd.DataFrame, features: list, target: str):
+def SSB_Factorial(data: pd.DataFrame, features: list, target: str) -> GroupStatistic:
+    """Return sum of squares, degrees of freedom and mean sum of squares between groups
+
+    Info:
+    -----
+    The SSB is a metric, that shows, how much the difference between our groups is. 
+    The Factorial SSB means, that we calculate the means for each feature we want to compare.
+    We then compare all the means with the general mean by squaring and summing all the values up.
+    So, we want to find out how much the mean of specific feature differs from the general mean.
+
+    Formula:
+    --------
+    * `sum_sq = Σ(ni(fm - GM)**2)`
+
+    where:
+        `sum_sq`: sum of squares
+        `ni`: number of elements in specific group
+        `fm`: feature mean
+        `GM`: grand mean
+
+    * `df = nf - 1`
+
+    where:
+        `df`: degrees of freedom
+        `nf`: number of features
+
+    * `ms = sum_sq / df`
+
+    where:
+        `ms`: mean sum of squares
+
+    -----
+    Args:
+        data (pd.DataFrame): data
+        features (list): feature list
+        target (str): dependent variable
+
+    Returns:
+        GroupStatistic: group statistic
+    """
+    features_sum_sq = []
+    features_df = []
+    features_ms = []
+    GM = np.mean(data[target].values)
+    
+    # For each feature...
+    for feature in features:
+        feature_means = [] # add up the means for each group of feature
+        feature_group = data.groupby(feature) # group by feature
+        # For each group with feature...
+        for _, feature_df in feature_group:
+            values = feature_df[target]
+            n = values.shape[0]
+            feature_mean = np.mean(values)
+            feature_means.append((n, feature_mean))
+
+        sum_sq = np.sum([n * (fm - GM)**2 for n, fm in feature_means])
+        df = data[feature].nunique() - 1
+        features_sum_sq.append(sum_sq)
+        features_df.append(df)
+        features_ms.append(sum_sq / df)
+        
+    return GroupStatistic(features_sum_sq, features_df, features_ms)
+
+def SSW_Factorial(data: pd.DataFrame, features: list, target: str) -> GroupStatistic:
+    """Return sum of squares, degrees of freedom and mean sum of squares within groups
+
+    Info:
+    -----
+    The process within groups means, that we split our data by features and groups in features.
+    For each group we calculate the same squared sum and trying to find out, how much the values
+    of group differs from the mean of group. If sum of squares of this differences not too much,
+    but SSB is big, we can assume we have strong difference in some feature.
+
+    Formula:
+    --------
+    * `sum_sq = Σ(ss)`
+    * `ss = Σ((vi - M)**2)`
+
+    where:
+        `sum_sq`: sum of squares
+        `ss`: sum of squares for specific group
+        `vi`: value in group
+        `M`: group mean
+
+    * `df = Σ(nfi - 1)`
+
+    where:
+        `df`: degrees of freedom
+        `nfi`: number of features for specific group
+
+    * `ms = sum_sq / df`
+
+    where:
+        `ms`: mean sum of squares
+
+    -----
+    Args:
+        data (pd.DataFrame): data
+        features (list): feature list
+        target (str): dependent variable
+
+    Returns:
+        GroupStatistic: group statistic
+    """
     groupped = data.groupby(features)
     ss_resid_list = []
     df_resid = 0
@@ -267,43 +363,156 @@ def SSW_pd(statistic: dict, data: pd.DataFrame, features: list, target: str):
         ss = np.sum([(vi - group_mean)**2 for vi in values])
         ss_resid_list.append(ss)
         df_resid += groupped_df.shape[0] - 1
-    ss_resid = sum(ss_resid_list)
-    statistic["feature"].append("residual")
-    statistic["sum_sq"].append(ss_resid)
-    statistic['df'].append(df_resid)
+    ss_resid = np.sum(ss_resid_list)
+    ms_resid = ss_resid / df_resid
     
-    return statistic, ss_resid, df_resid
+    return GroupStatistic(ss_resid, df_resid, ms_resid)
 
-def SSB_pd(data: pd.DataFrame, features: list, target: str):
-    statistic = {"feature": [], "sum_sq": [], "df": []}
-    ssb_features = []
-    df_ssb = []
-    grand_mean = np.mean(data["expr"].values)
-    # For each feature... [age, dose]
-    for feature in features:
-        feature_means = [] # add up the means for each group of feature
-        feature_group = data.groupby(feature) # group by feature (age)
-        # For each group with feature find the mean...
-        for _, feature_df in feature_group:
-            values = feature_df[target]
-            feature_mean = np.mean(values)
-            feature_means.append(feature_mean)
-        # Calculate the sum of squared difference 
-        # between the group mean and the gen mean
-        n = feature_df.shape[0]
-        sum_of_squares = np.sum([n * (fm - grand_mean)**2 for fm in feature_means])
+def interaction(total: float, ssb: GroupStatistic, ssw: GroupStatistic) -> GroupStatistic:
+    """Return feature interaction statistic
+
+    Info:
+    -----
+    The interaction means, how much variability explains by feature interaction.
+    To calculate the feature interaction we must subtract from total sum of squares the whole pack of calculations:
+    SSB for each feature and SSW (residuals). 
+    
+    If our SSB is low, SSW is low, 
+    but TTS is high, this means, that our variability of data may be explained by the interaction 
+    of our features. 
+
+    Formula:
+    --------
+    * `A ∩ B = TTS - SSB - SSW`
+
+    where:
+        `A ∩ B`: interaction between feature A and feature B
+        `TTS`: total sum of squares
+        `SSB`: sum of squares between groups
+        `SSW`: sum of squares within groups
+
+    * `df = ∏(ssb_df)`
+
+    where
+        `∏`: cumulative product of all degrees of freedom for each feature
+        `ssb_dt`: degrees of freedom for each feature
+
+    -----
+    Args:
+        total (float): total sum of squares
+        ssb (GroupStatistic): sum of squares between groups
+        ssw (GroupStatistic): sum of squares within groups
+
+    Returns:
+        GroupStatistic: interaction sum_sq, df and ms
+    """
+    ss = total - np.sum(ssb.sum_sq) - ssw.sum_sq
+    df = np.cumprod(ssb.df)[-1]
+    ms_interaction = ss / df
+    return GroupStatistic(ss, df, ms_interaction)
+
+def calculate_F_values(ssb: GroupStatistic, ssw: GroupStatistic, interactions: GroupStatistic) -> list:
+    """Calculate the F values
+
+    Info:
+    -----
+    To calculate the statistic value for our features, we need to divide each 'sum of squares' feature 
+    value between groups (SSB) onto the sum of squares within groups (SSW). The same process is valid for 
+    the 'interaction' sum_sq values. We can't get the negative value, because sum of squares is a 
+    strictly positive number, therefore here we have "F" distribution. Because if we don't have 
+    significant differences, then the mean of F values will be distributed around the 1 or something.
+
+    So, if our sum of squares between groups (SSB) is much larger than the sum of squares within groups (SSW), 
+    we can assume we have strong statistical significance. But this has to be verified with the pvalue calculation.
+
+    Formula:
+    --------
+    `f_value = MSSB / MSSW`
+
+    where:
+        `MSSB`: mean sum of squares between groups
+        `MSSW`: mean sum of squares within groups
+
+    -----
+    Args:
+        ssb (GroupStatistic): SSB statistic
+        ssw (GroupStatistic): SSW statistic
+        interactions (GroupStatistic): interaction statistic
+
+    Returns:
+        list: F values list
+    """
+    f_features = [ssb_ms_item / ssw.ms for ssb_ms_item in ssb.ms]
+    f_interaction = interactions.ms / ssw.ms
+    return [*f_features, f_interaction]
+
+def calculate_P_values(f_values: list, ssb: GroupStatistic, ssw: GroupStatistic) -> list:
+    """Calculate the P value for each feature and interaction F values
+
+    Info:
+    -----
+    To calculate P value of specific F statistic use f.sf method. 
+    "F" distribution is one tailed, so we don't need to multiply the value by 2 and passing 
+    absolute value of statistic, just like in case of "T" distribution.
+
+    To calculate P value of specific statistic, pass to f.sf method "F" statistic, 
+    degrees of freedom between groups (SSB.df) and degrees of freedom within groups (SSW.df).
+
+    The "sf" stands for Survival function and calculated as: (1 - cdf) at x of the given RV.
+    * RV is a random variables pack of specific distribution
+    * cdf is a cumulative distribution function, that return cumulative probability 
+    up to the specific ("F" value here) value
+
+    So, if we subtract the cdf from 1, we will get the cumulative probability of the chances, 
+    that our result has statistically significant differences. This is what called pvalue and
+    if the pvalue <= 0.05, we can assume we have significant sample differences.
+
+    Formula:
+    --------
+    * `p = f.sf(f_value, ssb, ssw)`
+    * `sf = 1 - cdf(f_value, ssb, ssw)`
+
+    -----
+    Args:
+        f_values (list): F values list
+        ssb (GroupStatistic): SSB statistic
+        ssw (GroupStatistic): SSW statistic
+
+    Returns:
+        list: P values list
+    """
+    return [f.sf(f_value, ssb.df, ssw.df)[-1] for f_value in f_values]
+
+def form_statistic(
+        ssb: GroupStatistic, 
+        ssw: GroupStatistic, 
+        interactions: GroupStatistic, 
+        f_values: list,
+        p_values: list,
+        features: list) -> pd.DataFrame:
+
+    statistic = {"feature": [], "sum_sq": [], "df": [], "F": [], "Pvalue": []}
+
+    # For each feature fill in corresponded values...
+    for sq, df, feature in zip(ssb.sum_sq, ssb.df, features):
         statistic['feature'].append(feature)
-        statistic['sum_sq'].append(sum_of_squares)
-        statistic['df'].append(len(features)-1)
-        ssb_features.append(sum_of_squares)
-        df_ssb.append(data[feature].nunique()-1)
-        
-    return statistic, ssb_features, df_ssb
+        statistic['sum_sq'].append(sq)
+        statistic['df'].append(df)
 
-def interaction(statistic, total, ssb, df_ssb, ssw, features):
-    ss_interaction = total - np.sum(ssb) - ssw
-    df = np.cumprod(df_ssb)[-1]
+    # Fill in values for interaction...
     statistic["feature"].append(":".join([*features]))
-    statistic["sum_sq"].append(ss_interaction)
-    statistic["df"].append(df)
-    return statistic, ss_interaction, df
+    statistic["sum_sq"].append(interactions.sum_sq)
+    statistic["df"].append(interactions.df)
+
+    # Fill in residuals values...
+    statistic["feature"].append("residual")
+    statistic["sum_sq"].append(ssw.sum_sq)
+    statistic['df'].append(ssw.df)
+
+    # Fill F and P values...
+    for f_val, p_val in zip(f_values, p_values):
+        statistic["F"].append(f_val)
+        statistic["Pvalue"].append(p_val)
+    
+    statistic = pd.DataFrame.from_dict(statistic, orient='index').T
+    return statistic
