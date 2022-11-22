c28577d1b4142458b648476e196b6597cb0bd4be
Pavel <statixx_err99@yahoo.com>
2022-11-22 12:50:44 +0300

    update example

--- machine_learning/examples/try_kmeans.py
+++ machine_learning/examples/try_kmeans.py
@@ -1,45 +1,42 @@
-from machine_learning.clustering.k_means import KMeans as kmeansmy
 from sklearn.cluster import KMeans
-import numpy as np
-import pandas as pd
+from machine_learning.clustering.k_means import KMeans as kmeansmy
 from time import time
+import numpy as np
 
-center_1 = np.array([1, 1])
-center_2 = np.array([5, 5])
-center_3 = np.array([8, 1])
 
-data_1 = np.random.randn(200, 2) + center_1
-data_2 = np.random.randn(200, 2) + center_2
-data_3 = np.random.randn(200, 2) + center_3
-
-df = np.concatenate((data_1, data_2, data_3), axis=0)
+X = np.array([[1, 2], [1, 4], [1, 0],
+              [10, 2], [10, 4], [10, 0]])
 
 start = time()
 print("[INFO] My KMEANS")
-mymodel = kmeansmy(data=df, n_clusters=3)
-mymodel.fit()
-print(mymodel.cluster_centers)
-print(mymodel.inertia)
+my_kmeans = kmeansmy(data=X, n_clusters=2).fit()
+print("[INFO] Labels:", my_kmeans.cluster_labels)
+print("[INFO] Predict:", my_kmeans.predict([[0, 0], [12, 3]]))
+print("[INFO] Centers:", my_kmeans.cluster_centers)
 print("[INFO] Time spent:", time() - start)
 
 print("\n")
+start = time()
 print("[INFO] Sklearn KMEANS")
-skmodel = KMeans(n_clusters=3)
-skmodel.fit(df)
-print(skmodel.cluster_centers_)
-print(skmodel.inertia_)
+kmeans = KMeans(n_clusters=2, random_state=0).fit(X)
+print("[INFO] Labels:", kmeans.labels_)
+print("[INFO] Predict:", kmeans.predict([[0, 0], [12, 3]]))
+print("[INFO] Centers:", kmeans.cluster_centers_)
+print("[INFO] Time spent:", time() - start)
 
 """
 [INFO] My KMEANS
-[[5.04600269 4.91269894]
- [8.05816874 0.921641  ]
- [0.99888886 1.02990041]]
-1130.0726820064901
+[INFO] Labels: [0 0 0 1 1 1]
+[INFO] Predict: [0 1]
+[INFO] Centers: [[ 1.  2.]
+ [10.  2.]]
+[INFO] Time spent: 0.005308866500854492
 
 
 [INFO] Sklearn KMEANS
-[[5.04600269 4.91269894]
- [0.99888886 1.02990041]
- [8.05816874 0.921641  ]]
-1130.0726820064904
+[INFO] Labels: [1 1 1 0 0 0]
+[INFO] Predict: [1 0]
+[INFO] Centers: [[10.  2.]
+ [ 1.  2.]]
+[INFO] Time spent: 0.10580325126647949
 """
