7195a4798ae047e06e07da66ec11c62f19ea44fc
Pavel <statixx_err99@yahoo.com>
2022-11-22 12:50:17 +0300

    added `predict` method

--- machine_learning/clustering/k_means.py
+++ machine_learning/clustering/k_means.py
@@ -11,6 +11,21 @@ class KMeans:
                  n_clusters: int,
                  n_init: int = 10,
                  tolerance: float = 1e-3):
+        """
+        K-means is the unsupervised learning method, which clusterize
+        the data points using just the distance between points.
+
+        cluster_centers: contain, where each cluster is located
+        previous_cluster_centers: previous position of clusters to check the difference later
+        inertia: the global distance of each point to their cluster (more info in `__get_inertia()`
+        cluster_labels: contains the indices of cluster with minimum distance to specified point
+
+        Args:
+            data: array of data
+            n_clusters: how many clusters to set
+            n_init: how many times the algorithm will be run
+            tolerance: error for distance difference
+        """
         self.data = data
         self.n_clusters = n_clusters
         self.n_init = n_init
@@ -25,13 +40,19 @@ class KMeans:
         self.cluster_centers = self._initialize_clusters()
         self.previous_cluster_centers = copy.deepcopy(self.cluster_centers)
         self.inertia = np.inf
+        self.cluster_labels = None
 
     def fit(self):
+        """
+        This method runs KMeans algorithm `n_init` times to get better fitting
+        result and avoid bad random position, when clusters are initialized
+        """
         for i in range(self.n_init):
-            cluster_centers, inertia, cluster_indices = self.fit_iter()
+            cluster_centers, inertia, cluster_labels = self.fit_iter()
             if inertia < self.inertia:
                 self.inertia = inertia
                 self.cluster_centers = cluster_centers
+                self.cluster_labels = cluster_labels
 
         return self
 
@@ -41,7 +62,7 @@ class KMeans:
 
         Steps:
         ------
-        1. Initialize the cluster centers with random values (did it in init method)
+        1. Initialize the cluster centers with random values
         2. Calculate the distance between data and cluster centers
         3. For each point assign the closest cluster
         4. Calculate centroids for each data by clusters
@@ -81,28 +102,26 @@ class KMeans:
         inertia = self._get_inertia(centroids, assigned_classes)
         return centroids, inertia, assigned_classes
 
-    def _get_inertia(self, centroids, assigned_classes) -> float:
+    def predict(self, x: Union[list, np.ndarray]) -> np.ndarray:
         """
-        Inertia is the metric that show how far our data from their clusters.
+        Return cluster indices for passed array of data
 
         Steps:
         ------
-        To calculate inertia:\n
-        1. Calculate Euclidian distance between data and centroid\n
-        2. Square this distance\n
-        3. Sum it all together\n
+        To clustering the passed data we need: \n
+        1. Find the distance for each point and all clusters
+        2. Return the indices with minimal distance in distance matrix
 
-        The lower the inertia -> the better the model.
+        Args:
+            x: (np.ndarray) array
+
+        Returns:
+            indexes corresponding to the closest cluster
         """
-        distances_by_clusters = []
-        for i in range(self.n_clusters):
-            cluster_data = self.data[assigned_classes == i]
-            centroid = centroids[i]
-            distance = euclidean_distance(cluster_data, centroid)
-            squared_distance = np.power(distance, 2)
-            distances_by_clusters.append(squared_distance)
-
-        return np.array(distances_by_clusters).sum()
+        x = np.array(x)
+        distances = self._calculate_euclidian_distance(x, self.cluster_centers)
+        labels = self._assign_cluster_to_data(distances)
+        return labels
 
     def _initialize_clusters(self) -> np.ndarray:
         """
@@ -117,13 +136,13 @@ class KMeans:
         cluster_centers = np.random.randn(self.n_clusters, dim) * std + mean
         return cluster_centers
 
-    def _initialize_distance_matrix(self):
+    def _initialize_distance_matrix(self, data):
         """
         Return the array, that will contain the distance for each cluster
         between data point and cluster point. Shape: (number of rows, number of clusters).
         This means that for each row we'll write the distance to each cluster
         """
-        n_rows = self.data.shape[0]
+        n_rows = data.shape[0]
         return np.zeros(shape=(n_rows, self.n_clusters))
 
     def _calculate_euclidian_distance(self, data, clusters, fast_method: bool = True) -> np.ndarray:
@@ -131,7 +150,7 @@ class KMeans:
         Here we calculate the distance between each data point (the row) and
         the cluster point. Each value are written in the `distance` variable.
         """
-        distances = self._initialize_distance_matrix()
+        distances = self._initialize_distance_matrix(data)
 
         if fast_method:
             for i in range(self.n_clusters):
@@ -156,6 +175,9 @@ class KMeans:
         centroids = np.zeros_like(self.cluster_centers)
         for i in range(self.n_clusters):
             cluster_data = self.data[assigned_classes == i]
+            # Avoid mean calculation, when array is empty
+            if cluster_data.shape[0] == 0:
+                return np.NaN
             centroids[i] = np.mean(cluster_data, axis=0)
         return centroids
 
@@ -167,3 +189,37 @@ class KMeans:
     def _get_distance_diff(self, current, previous):
         """Return the difference between the previous centers and current centers"""
         return np.linalg.norm(current - previous)
+
+    def _get_inertia(self, centroids, assigned_classes, fast_method: bool = True) -> float:
+        """
+        Inertia is the metric that show how far our data from their clusters.
+
+        Steps:
+        ------
+        To calculate inertia: \n
+        1. Calculate Euclidian distance between data and centroid \n
+        2. Square this distance \n
+        3. Sum it all together \n
+
+        The lower the inertia -> the better the model.
+        """
+        if fast_method:
+            distance_by_clusters = np.zeros(shape=(self.n_clusters, 1))
+            for i in range(self.n_clusters):
+                cluster_data = self.data[assigned_classes == i]
+                centroid = centroids[i]
+                norm = np.linalg.norm(cluster_data - centroid, axis=1)
+                distance_by_clusters[i] = norm.__pow__(2).sum()
+
+            return distance_by_clusters.sum()
+
+        else:
+            distances_by_clusters = []
+            for i in range(self.n_clusters):
+                cluster_data = self.data[assigned_classes == i]
+                centroid = centroids[i]
+                distance = euclidean_distance(cluster_data, centroid)
+                squared_distance = np.power(distance, 2)
+                distances_by_clusters.append(squared_distance)
+
+            return np.array(distances_by_clusters).sum()
